0bd7e820b51f840f8430ffc4f6067bed062be25b
paalmoest <paalmm@tihlde.org>
2010-10-04 12:21:00 +0200

    ny test

--- mathplay/src/java/mathplay/UserBean.java
+++ mathplay/src/java/mathplay/UserBean.java
@@ -5,10 +5,30 @@
 
 package mathplay;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
 /**
  *
  * @author ikkedinpc
  */
+@ManagedBean
+@SessionScoped
 public class UserBean {
+    public String name;
+    public String pw;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+    public String getPw() {
+        return pw;
+    }
 
 }
