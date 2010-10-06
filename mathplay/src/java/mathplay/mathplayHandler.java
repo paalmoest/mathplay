dc3967f23bb3fed4a0c2f344bda856fd3a4ead08
Stian Sorebo <stian.soerebo@gmail.com>
2010-10-06 13:43:01 +0200

    Signed-off-by: Stian Sorebo <stian.soerebo@gmail.com>

--- mathplay/src/java/mathplay/mathplayHandler.java
+++ mathplay/src/java/mathplay/mathplayHandler.java
@@ -4,6 +4,10 @@
  */
 
 package mathplay;
+import java.util.ArrayList;
+import java.lang.*;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -11,4 +15,31 @@ package mathplay;
  */
 public class mathplayHandler {
 
+    private String password;
+    private String username;
+
+
+
+
+
+    public String getUsername(){
+        return username;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+
+
+
 }
+
