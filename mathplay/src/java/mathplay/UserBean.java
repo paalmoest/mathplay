54de01e7e1c1a3f7c72e6bd9876cf16e02a2bda7
oyvindse <oyvind.selmer@gmail.com>
2010-10-08 02:31:48 -0700

    Removed unused methods and translated to english.

--- mathplay/src/java/mathplay/UserBean.java
+++ mathplay/src/java/mathplay/UserBean.java
@@ -4,94 +4,64 @@
  */
 
 package mathplay;
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import java.util.logging.Logger;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.*;
 import javax.servlet.http.*;
-
-/**
- *
- * @author ikkedinpc
- */
-
 
 @ManagedBean
 @SessionScoped
-  public class UserBean {
-     private String name;
-     private String role;
-     private Logger logger = Logger.getLogger("oving8");
-     private String rolle;
-     private String navn;
+public class UserBean {
+     private String name, userName, role, grade;
+  
+     public UserBean(String name, String userName, String role, String grade) {
+		 this.name = name;
+                 this.userName = userName;
+		 this.role = role;
+                 this.grade = grade;
+     }
 
-     public UserBean(String sNavn, String sRolle) {
-		 navn = sNavn;
-		 rolle = sRolle;
-	 }
-    public UserBean() {
-	 }
+     public UserBean() {
+     }
 
+     /** Property: name */
      public String getName() {
-        if (name == null) getUserData();
-        return name == null ? "" : name;
+         return name;
      }
 
-     public String getRole() { return role == null ? "" : role; }
-     public void setRole(String newValue) { role = newValue; }
-
-     public boolean isInRole() {
-        ExternalContext context
-           = FacesContext.getCurrentInstance().getExternalContext();
-        Object requestObject =  context.getRequest();
-        if (!(requestObject instanceof HttpServletRequest)) {
-           logger.severe("request object has type " + requestObject.getClass());
-           return false;
-        }
-       HttpServletRequest request = (HttpServletRequest) requestObject;
-       return request.isUserInRole(role);
-    }
-
-     private void getUserData() {
-        ExternalContext context
-           = FacesContext.getCurrentInstance().getExternalContext();
-        Object requestObject =  context.getRequest();
-        if (!(requestObject instanceof HttpServletRequest)) {
-           logger.severe("request object has type " + requestObject.getClass());
-           return;
-        }
-        HttpServletRequest request = (HttpServletRequest) requestObject;
-        name = request.getRemoteUser();
+     public void setName(String name) {
+         this.name = name;
      }
 
-     public String getUser() {
-		ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
-		Object requestObject =  context.getRequest();
-		HttpServletRequest request = (HttpServletRequest) requestObject;
-		name = request.getRemoteUser();
-		return name;
-	}
-	public String getRolle() {
-		return rolle;
-	}
+      /** Property userName */
+     public String getUserName() {
+         return userName;
+     }
 
-	public void setRolle(String nyRolle) {
-		rolle = nyRolle;
-	}
+     public void setUserName(String userName) {
+         this.userName = userName;
+     }
 
-	public String getNavn() {
-		return navn;
-	}
+     /** Property: role */
+     public String getRole() {
+         return role;
+     }
 
-	public void setNavn(String nyNavn) {
-		navn = nyNavn;
-	}
-	public synchronized String logout() throws Exception
-	 {
+     public void setRole(String role) {
+         this.role = role;
+     }
+
+     /** Property: grade */
+     public String getGrade() {
+         return grade;
+     }
+
+     public void setGrade(String grade) {
+         this.grade = grade;
+     }
+     
+     public synchronized String logout() throws Exception {
 	  ExternalContext ectx = FacesContext.getCurrentInstance().getExternalContext();
 	  HttpSession session = (HttpSession) ectx.getSession(false);
 	  session.invalidate();
